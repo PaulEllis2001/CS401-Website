b79a57cfef16b723c79821f16bfe1c7f28dd09ee
PaulEllis2001 <paul@pdellis.com>
2023-02-10 12:53:47 -0700

    Added basic table

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
             <div id="leaderboardSummaryHeaders">
                 <div class="left_box">
                     <h2 class="boxHeading">Top Gain</h2>
+                    <table>
+                        <thead>
+                            <th>
+                                Column 1
+                            </th>
+                            <th>
+                                Column 2
+                            </th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Hello</td>
+                                <td>World</td>
+                            </tr>
+                            <tr>
+                                <td>This is</td>
+                                <td>Paul</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div class="right_box">
                     <h2 class="boxHeading">Top Loss</h2>
