fde31cbf19cb5da6609d7544bd394933213c4460
PaulEllis2001 <paul@pdellis.com>
2023-02-10 10:56:29 -0700

    work on the home page boxes

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 <h1>Welcome!</h1>
 
+<div>
+    <div class="flex-row-box">
+        <div id="leaderboardSummary" class="info_box">
+            <div id="leaderboardSummaryHeaders">
+                <h2 class="boxHeading">Top Gain</h2>
+                <h2 class="boxHeading">Top Loss</h2>
+            </div>
+        </div>
+        <div id="coinSummary" class="info_box">
+
+        </div>
+    </div>
+    <div id="accountSummary" class="info_box">
+
+    </div>
+
+</div>
+
 <?php include("includes/footer.php"); ?>
