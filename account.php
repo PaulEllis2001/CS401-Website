879990e1d4627c10d022b16f656cb64dd9442598
PaulEllis2001 <paul@pdellis.com>
2023-02-16 14:49:26 -0700

    input dummy data into account

--- account.php
+++ account.php
@@ -6,13 +6,128 @@
         <div class="row">
             <div>
                 <!-- PORTFOLIO OVERVIEW -->
+                <h2>Portfolio Overview</h2>
+                <p>Current Cash: $ 32,100</p>
+                <p>Current Invested: $ 2,950.10</p>
+                <p>24 Hour Change:</p><p> + .03%</p>
+                <p>Top Value Coin: Coiny Coin</p>
+                <p>Bottom Value Coin: AlphaBravo Coin</p>
+                <p>Current Rank: #321</p>
             </div>
             <div>
                 <!-- COINS IN WALLET  -->
+                <h2>Coins In Wallet</h2>
+                <table>
+                    <thead>
+                        <th>Name</th>
+                        <th>Ammount</th>
+                        <th>Value</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>CoinyCoin</td>
+                            <td>121</td>
+                            <td>$ 1,234.56</td>
+                        </tr>
+                        <tr>
+                            <td>CoinyCoin</td>
+                            <td>121</td>
+                            <td>$ 1,234.56</td>
+                        </tr>
+                        <tr>
+                            <td>CoinyCoin</td>
+                            <td>121</td>
+                            <td>$ 1,234.56</td>
+                        </tr>
+                        <tr>
+                            <td>CoinyCoin</td>
+                            <td>121</td>
+                            <td>$ 1,234.56</td>
+                        </tr>
+                        <tr>
+                            <td>CoinyCoin</td>
+                            <td>121</td>
+                            <td>$ 1,234.56</td>
+                        </tr>
+                        <tr>
+                            <td>CoinyCoin</td>
+                            <td>121</td>
+                            <td>$ 1,234.56</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
         <div>
             <!-- HISTORY TABLE -->
+            <h2>History</h2>
+            <table>
+                <thead>
+                    <th>Coin</th>
+                    <th>Ammount</th>
+                    <th>Value</th>
+                    <th>Gain/Loss</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>AlphaBravo Coin</td>
+                        <td>200</td>
+                        <td>$30.10</td>
+                        <td>-$19.90</td>
+                        <td>Sell</td>
+                    </tr>
+                    <tr>
+                        <td>Coiny Coin</td>
+                        <td>121</td>
+                        <td>$ 1,234.56</td>
+                        <td>-----</td>
+                        <td>Buy</td>
+                    </tr>
+                    <tr>
+                        <td>AlphaBravo Coin</td>
+                        <td>200</td>
+                        <td>$30.10</td>
+                        <td>-$19.90</td>
+                        <td>Sell</td>
+                    </tr>
+                    <tr>
+                        <td>Coiny Coin</td>
+                        <td>121</td>
+                        <td>$ 1,234.56</td>
+                        <td>-----</td>
+                        <td>Buy</td>
+                    </tr>
+                    <tr>
+                        <td>AlphaBravo Coin</td>
+                        <td>200</td>
+                        <td>$30.10</td>
+                        <td>-$19.90</td>
+                        <td>Sell</td>
+                    </tr>
+                    <tr>
+                        <td>Coiny Coin</td>
+                        <td>121</td>
+                        <td>$ 1,234.56</td>
+                        <td>-----</td>
+                        <td>Buy</td>
+                    </tr>
+                    <tr>
+                        <td>AlphaBravo Coin</td>
+                        <td>200</td>
+                        <td>$30.10</td>
+                        <td>-$19.90</td>
+                        <td>Sell</td>
+                    </tr>
+                    <tr>
+                        <td>Coiny Coin</td>
+                        <td>121</td>
+                        <td>$ 1,234.56</td>
+                        <td>-----</td>
+                        <td>Buy</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
