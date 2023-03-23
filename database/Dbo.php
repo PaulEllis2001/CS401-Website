0ec00f1ce4bac6e83f1e462406199aae1e711a15
PaulEllis2001 <paul@pdellis.com>
2023-03-22 21:24:37 -0600

    Work on DB

--- database/Dbo.php
+++ database/Dbo.php
@@ -1,12 +1,21 @@
 <?php
 
-    class Dbo{
+    class Dao{
 
-        private $db;
-        private $user;
-        private $password;
-        private $fourth;
+        private $db = "FraudCoinDB";
+        private $user = "root";
+        private $password = $_ENV["DB_PASSWORD"];
+        private $host = "localhost";
 
+        public function getConnection(){
+            return new PDO("mysql:host={$this->host};dbname={$this->db}",
+            $this->user, $this->password);
+        }
+
+        public function getCoinSummary(){
+            $connection = $this->getConnection();
+            return $connection->query("SELECT * from coin_history WHERE change_time")->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 
 
