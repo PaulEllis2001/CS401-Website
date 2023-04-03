32b56db27f301d5b33d60faa5fe8c275175bc403
PaulEllis2001 <paul@pdellis.com>
2023-04-02 20:40:30 -0600

    debugging

--- handlers/login_handler.php
+++ handlers/login_handler.php
@@ -4,17 +4,18 @@ require_once '../database/Dao.php';
 $dao = new Dao();
 
 if(isset($_POST["create_username"])){
-    $dao->createNewUser($_POST["create_username"], 
+    $response = $dao->createNewUser($_POST["create_username"], 
     $_POST["create_password"], 
     $_POST["create_email"], 
     $_POST["create_birthday"]);
+
+    $_COOKIE["response"] = $response;
 }
 
 if(isset($_POST["login_username"])){
     $response = $dao->getLoginInformation($_POST["login_username"]);
     //VERIFY LOGIN INFORMATION
     if( isset( $response["user_password"] ) ){
-        
     }
 }
 
--- test.php
+++ test.php
@@ -5,32 +5,12 @@ $dao = new Dao();
 
 $result = $dao->debugUsers();
 
-print_r($result)
+print_r($result, 1)
 ?>
 
-<form class="form_box login_box" id="create_account_form" method="POST" action="test.php">
-    <lable class="form_item" for="create_username">Select a Username:</lable>
-    <input class="form_item" type="text" id="create_username" name="create_username">
-    <label class="form_item" for="create_email">Enter an Email:</label>
-    <input class="form_item" type="email" id="create_email" name="create_email">
-    <label class="form_item" for="create_birthdate">Enter Birthday:</label>
-    <input class="form_item" type="date" id="create_birthday" name="create_birthday">
-    <label class="form_item" for="create_password">Select a Password:</label>
-    <input class="form_item" type="password" id="create_password" name="create_password">
-    <label class="form_item" for="create_validate_password">Confirm Password:</label>
-    <input class="form_item" type="password" id="create_validate_password" name="create_validate_password">
-    <input class="form_item login_button" type="submit" value="Create Account">
-</form>
 
 <?php 
-    if(isset($_POST["create_birthday"])){
-        echo $_POST["create_birthday"];
 
-        $result = $dao->createNewUser($_POST["create_username"], $_POST["create_password"],
-        $_POST["create_email"], $_POST["create_birthday"]);
-
-        echo $result;
-    }
 
 
 ?>
