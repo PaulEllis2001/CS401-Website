ff284835ad93e6f7c43441acedb02f4b0ca3f5fb
PaulEllis2001 <paul@pdellis.com>
2023-04-02 20:21:27 -0600

    more testing

--- test.php
+++ test.php
@@ -7,3 +7,23 @@ $result = $dao->debugUsers();
 
 print_r($result)
 ?>
+
+<form class="form_box login_box" id="create_account_form" method="POST" action="test.php">
+    <lable class="form_item" for="create_username">Select a Username:</lable>
+    <input class="form_item" type="text" id="create_username" name="create_username">
+    <label class="form_item" for="create_email">Enter an Email:</label>
+    <input class="form_item" type="email" id="create_email" name="create_email">
+    <label class="form_item" for="create_birthdate">Enter Birthday:</label>
+    <input class="form_item" type="date" id="create_birthday" name="create_birthday">
+    <label class="form_item" for="create_password">Select a Password:</label>
+    <input class="form_item" type="password" id="create_password" name="create_password">
+    <label class="form_item" for="create_validate_password">Confirm Password:</label>
+    <input class="form_item" type="password" id="create_validate_password" name="create_validate_password">
+    <input class="form_item login_button" type="submit" value="Create Account">
+</form>
+
+<?php 
+    if(isset($_POST["create_birthday"])){
+        echo $_POST["create_birthday"];
+    }
+?>
