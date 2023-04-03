8a92eaead2d45693929ad1ed33f729f853862559
Paul Ellis <paul@pdellis.com>
2023-04-03 10:14:12 -0600

    more testing

--- test.php
+++ test.php
@@ -9,6 +9,11 @@ echo "<pre>" . print_r($result, 1) . "</pre>";
 
 $coins = $dao->debugTable("coin");
 echo "<pre>" . print_r($coins, 1) . "</pre>";
+
+$history = $dao->getCoinSummary();
+
+echo "<pre>" . print_r($history, 1) . "</pre>";
+
 ?>
 
 
