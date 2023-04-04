341788e04b981e8bdde432bb534ad0ee93f87dde
PaulEllis2001 <paul@pdellis.com>
2023-04-03 20:26:36 -0600

    sadf

--- login.php
+++ login.php
@@ -12,11 +12,11 @@ function oldUsername(){
         <h2>Create Account</h2>
         <form class="form_box login_box" id="create_account_form" method="POST" action="handlers/create_account_handler.php">
             <lable class="form_item" for="create_username">Select a Username:</lable>
-            <input class="form_item" value="<?php echo isset($_SESSION['create']) ? "\"" . json_decode($_SESSION['prev_info'])['create_username']. "\"" : ""; ?>" type="text" id="create_username" name="create_username">
+            <input class="form_item" value="<?php echo isset($_SESSION['create']) ?  json_decode($_SESSION['prev_info'])['create_username'] : ""; ?>" type="text" id="create_username" name="create_username">
             <label class="form_item" for="create_email">Enter an Email:</label>
-            <input class="form_item" value="<?php echo isset($_SESSION['create']) ? "\"" . json_decode($_SESSION['prev_info'])['create_email']. "\"" : ""; ?>" type="email" id="create_email" name="create_email">
+            <input class="form_item" value="<?php echo isset($_SESSION['create']) ?  json_decode($_SESSION['prev_info'])['create_email'] : ""; ?>" type="email" id="create_email" name="create_email">
             <label class="form_item" for="create_birthdate">Enter Birthday:</label>
-            <input class="form_item" value="<?php echo isset($_SESSION['create']) ? "\"" . json_decode($_SESSION['prev_info'])['create_birthday'] . "\"" : ""; ?>" type="date" id="create_birthday" name="create_birthday">
+            <input class="form_item" value="<?php echo isset($_SESSION['create']) ?  json_decode($_SESSION['prev_info'])['create_birthday']  : ""; ?>" type="date" id="create_birthday" name="create_birthday">
             <label class="form_item" for="create_password">Select a Password:</label>
             <input class="form_item" type="password" id="create_password" name="create_password">
             <label class="form_item" for="create_validate_password">Confirm Password:</label>
@@ -37,7 +37,7 @@ if(isset($_SESSION['message'])){
         <h2>Login</h2>
         <form class="form_box login_box" id="login_form" method="POST" action="handlers/login_handler.php">
             <label class="form_item" for="login_username">Username:</label>
-            <input class="form_item" value="<?php echo isset($_SESSION['failure']['login']) ? "\"" .  oldUsername() . "\"": ""; ?>" type="text" id="login_username" name="login_username">
+            <input class="form_item" value="<?php echo isset($_SESSION['failure']['login']) ? oldUsername() : ""; ?>" type="text" id="login_username" name="login_username">
             <label class="form_item" for="login_password">Password:</label>
             <input class="form_item" type="password" id="login_password" name="login_password">
             <input class="form_item login_button" type="submit" value="Login">
